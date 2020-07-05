cba3457cf384c2445aa212208ae0ef0ca113c6a3
tgtubbs <tgtubbs@icloud.com>
2020-07-05 17:38:30 -0400

    Get nba team schedules (closes #3)

--- espn_fantasy_basketball_client/client.py
+++ espn_fantasy_basketball_client/client.py
@@ -1,4 +1,5 @@
 from session import Session
+from typing import Dict
 
 
 class Client:
@@ -7,3 +8,17 @@ class Client:
         self.base_url = "https://fantasy.espn.com/apis/v3/games/fba"
         self.session = Session()
         self.session.authenticate(username=username, password=password)
+
+    @staticmethod
+    def _inspect_response(response):
+        if response.status_code == 200:
+            return
+        else:
+            raise Exception(f"Request failed with status {response.status}")
+    
+    def get_nba_team_schedules(self, season: int) -> Dict:
+        url = f"{self.base_url}/seasons/2020"
+        params = {"view": ["proTeamSchedules_wl"]}
+        response = self.session.get(url=url, params=params)
+        self._inspect_response(response)
+        return response.json()["settings"]["proTeams"]
