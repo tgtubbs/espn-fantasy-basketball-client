f355c26aa66ed505fbaefcf7222dbb345ef91be5
tgtubbs <tgtubbs@icloud.com>
2020-07-06 18:40:55 -0400

    re-use cookies across sessions

--- espn_fantasy_basketball_client/session.py
+++ espn_fantasy_basketball_client/session.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 import requests
 import yaml
 
@@ -11,23 +13,37 @@ class Session(requests.Session):
         self.headers.update({"ContentType": "application/json"})
     
     def authenticate(self, username: str, password: str):
-        '''
-        Authenticate using username and password
-        Add cookies to session
-        '''
+        """
+        1. Add API key to session headers
+        2. If cookies from a previous session exist, load them.
+        3. If cookies from a previous session don't exist or have expired,
+           authenticate with usename and password, save cookies.
+        """
         self._get_api_key()  # Adds api key to session headers
-        response = self.post(url=self.login_url, json={"loginValue": username, "password": password}).json()
-        requests.utils.add_dict_to_cookiejar(
-            cj=self.cookies,
-            cookie_dict={
-                "espn_s2": response["data"]["s2"], 
-                "swid": response["data"]["profile"]["swid"]
-            }
-        )
-    
-    def _get_api_key(self):
-        '''
-        Add API key to session headers
-        '''
+        if self._cookies_exist():
+            self.cookies = self._load_cookies()
+        else:
+            response = self.post(url=self.login_url, json={"loginValue": username, "password": password}).json()
+            requests.utils.add_dict_to_cookiejar(
+                cj=self.cookies,
+                cookie_dict={
+                    "espn_s2": response["data"]["s2"], 
+                    "swid": response["data"]["profile"]["swid"]
+                }
+            )
+            self._save_cookies()
+
+    def _get_api_key(self) -> None:
         response = self.post(url=self.api_key_url)
         self.headers.update({"authorization": f"APIKEY {response.headers['api-key']}"})
+
+    def _save_cookies(self) -> None:
+        with open("cookies.pkl", "wb") as file:
+            pickle.dump(self.cookies, file)
+
+    def _load_cookies(self) -> requests.cookies:
+        with open("cookies.pkl", "rb") as file:
+            return pickle.load(file)
+
+    def _cookies_exist(self) -> bool:
+        return os.path.exists("cookies.pkl")
