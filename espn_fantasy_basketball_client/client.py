c652a165463bacee07759435165280e4a3cfb93d
tgtubbs <tgtubbs@icloud.com>
2020-07-07 17:28:32 -0400

    get league members

--- espn_fantasy_basketball_client/client.py
+++ espn_fantasy_basketball_client/client.py
@@ -1,14 +1,14 @@
 from session import Session
-from typing import Dict
+from typing import Dict, List
 
 
 class Client:
 
     def __init__(self, username: str, password: str, league_id: int):
         self.base_url = "https://fantasy.espn.com/apis/v3/games/fba"
+        self.league_id = league_id
         self.session = Session()
         self.session.authenticate(username=username, password=password)
-        self.league_id = league_id
 
     @staticmethod
     def _inspect_response(response):
@@ -37,3 +37,9 @@ class Client:
         response = self.session.get(url=url, params=params)
         self._inspect_response(response)
         return response.json()
+
+    def get_league_members(self, season: int) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
+        response = self.session.get(url=url)
+        self._inspect_response(response)
+        return response.json()["members"]
