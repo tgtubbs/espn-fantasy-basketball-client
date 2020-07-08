7da1fa96dc47efefbed2d5bdeb1c5ffe3c1f5ca5
tgtubbs <tgtubbs@icloud.com>
2020-07-07 22:59:25 -0400

    make season a client property, change _get to accomodate custom headers

--- espn_fantasy_basketball_client/client.py
+++ espn_fantasy_basketball_client/client.py
@@ -8,6 +8,7 @@ class Client:
     def __init__(self, username: str, password: str, league_id: int):
         self.base_url = "https://fantasy.espn.com/apis/v3/games/fba"
         self.league_id = league_id
+        self.season = season
         self.session = Session()
         self.session.authenticate(username=username, password=password)
 
@@ -18,57 +19,69 @@ class Client:
         else:
             raise Exception(f"Request failed with status {response.status}")
     
-    def _get(self, url: str, **kwargs) -> requests.models.Response:
-        response = self.session.get(url=url, params=kwargs)
+    def _get(self, url: str, params: dict = None, headers: dict = None) -> requests.models.Response:
+        response = self.session.get(url=url, params=params, headers=headers)
         self._inspect_response(response)
         return response
 
-    def get_nba_team_schedules(self, season: int) -> List[Dict]:
-        url = f"{self.base_url}/seasons/{season}"
-        response = self._get(url=url, view="proTeamSchedules_wl")
+    def get_nba_team_schedules(self) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{self.season}"
+        params = {"view": "proTeamSchedules_wl"}
+        response = self._get(url=url, params=params)
         return response.json()["settings"]["proTeams"]
 
     def get_league_history(self) -> List[Dict]:
         url = f"{self.base_url}/leagueHistory/{self.league_id}"
-        response = self._get(url=url, view="kona_history_standings")
+        params = {"view": "kona_history_standings"}
+        response = self._get(url=url, params=params)
         return response.json()
 
-    def get_league_members(self, season: int) -> List[Dict]:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
+    def get_league_status(self) -> Dict:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
+        params = {"view": "mStatus"}
+        response = self._get(url=url, params=params)
+        return response.json()["status"]
+
+    def get_members(self) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
         response = self._get(url=url)
         return response.json()["members"]
 
-    def get_league_status(self, season: int) -> Dict:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
-        response = self._get(url=url, view="mStatus")
-        return response.json()["status"]
-
-    def get_league_matchup_scores(self, season: int) -> List[Dict]:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
-        response = self._get(url=url, view="mMatchupScore")
+    def get_matchup_scores(self) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
+        params = {"view": "mMatchupScore"}
+        response = self._get(url=url, params=params)
         return response.json()["schedule"]
     
-    def get_league_matchup_stats(self, season: int) -> List[Dict]:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
-        response = self._get(url=url, view="mScoreboard")
+    def get_matchup_stats(self) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
+        params = {"view": "mScoreboard"}
+        response = self._get(url=url, params=params)
         return response.json()["schedule"]
     
-    def get_league_standings(self, season: int) -> List[Dict]:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
-        response = self._get(url=url, view="mScoreboard")
+    def get_standings(self) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
+        params = {"view": "mScoreboard"}
+        response = self._get(url=url, params=params)
         return response.json()["teams"]
 
-    def get_league_settings(self, season: int) -> Dict:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
-        response = self._get(url=url, view="mSettings")
+    def get_league_settings(self) -> Dict:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
+        params = {"view": "mSettings"}
+        response = self._get(url=url, params=params)
         return response.json()["settings"]
-
-    def get_league_players(self, season: int) -> List[Dict]:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
-        response = self._get(url=url, view="kona_player_info")
-        return response.json()["players"]
     
-    def get_team_roster(self, season: int, team_id: int) -> List[Dict]:
-        url = f"{self.base_url}/seasons/{season}/segments/0/leagues/{self.league_id}"
+    def get_teams(self) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
+        response = self._get(url=url)
+        return response.json()["teams"]
+
+    def get_team(self, team_id: int) -> Dict:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
+        response = self._get(url=url, forTeamId=team_id)
+        return response.json()["teams"][0]
+
+    def get_team_roster(self, team_id: int) -> List[Dict]:
+        url = f"{self.base_url}/seasons/{self.season}/segments/0/leagues/{self.league_id}"
         response = self._get(url=url, view="mRoster", forTeamId=team_id)
         return response.json()["teams"][0]["roster"]["entries"]
